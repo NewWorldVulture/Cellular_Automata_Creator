aa353fa8f4d62395d88d0508a14a719623307655
NewWorldVulture <49806198+NewWorldVulture@users.noreply.github.com>
2019-12-21 04:46:27 -0800

    Added more colors
    We've reached a point where I clearly need to stop, but I keep getting "ideas". it's 4am, what do you want from me.

--- Cell_Automata.py
+++ Cell_Automata.py
@@ -2,30 +2,47 @@ from PIL import Image
 import PIL
 import itertools
 import random
+import math
 
 size = 500
 
 rule = 110
 # please only input odds. I don't know if/how evens work.
+
 input_size = 3
+number_of_colors = 2
 
 # turns the rules
-size = (2*size+2, size+1)
-rule_bin = list(format(rule, f'#0{2**input_size+2}b')[2:])
-rule_bin = rule_bin[::-1]
-# Produces all binary digits that are `input_size` in length in order...
-# ...in a list like so: `['000', '001', '010', '011', ...]`
-keys = list(map(''.join, itertools.product('01', repeat=input_size)))
+size = ((input_size//2+1)*size+2, size+1)
+
+def convert_to_base(n, base):
+    if n == 0:
+        return '0'
+    nums = []
+    while n:
+        n, r = divmod(n, base)
+        nums.append(str(r))
+    return ''.join(nums)[::-1].zfill(number_of_colors**input_size+number_of_colors)[::-1]
+
+rule_in_base = convert_to_base(rule, number_of_colors)
+print(rule_in_base)
+#rule_bin = list(format(rule, f'#0{2**input_size+2}b')[2:])
+rule_formatted = list(rule_in_base)
+# Produces all n-ary digits that are `input_size` in length in order...
+# ...into a list like so: `['000', '001', '010', '011', ...]`
+numbers = ''.join([str(x) for x in range(number_of_colors)])
+keys = list(map(''.join, itertools.product(numbers, repeat=input_size)))
 
 # Maps the keys produced to the digits of `rule_binary`
-rule_rules = dict(zip(keys, rule_bin))
+rule_rules = dict(zip(keys, rule_formatted))
+print(rule_rules)
 
 width, height = size
 cells = [['0' for _ in range(width)] for _ in range(height)]
 origin = int(width/2)+1
 cells[0][origin] = '1'
 #cells[0] = [random.choice(["0","1"]) for _ in range(width)]
-colors = [(0, 0, 0), (255, 255, 255)]
+colors = [(0, 0, 0), (255, 255, 255), (255, 0, 0), (255, 255, 0), (255, 0, 255)]
 
 def decide_rule(row, cell):
     rule_input = ''.join(cells[row-input_size//2][cell-input_size//2:cell+input_size//2+1])
@@ -42,5 +59,5 @@ pixels = im.load()
 for r in range(height-1):
     for c in range(width-input_size//2-1):
         pixels[c, r] = colors[int(cells[r+1][c+1])]
-im.save(f"rule{rule}.png")
+im.save(f"rule{rule}_{input_size}_bit.png")
 im.show()
