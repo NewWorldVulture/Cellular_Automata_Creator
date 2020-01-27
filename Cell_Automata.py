eaa68b64917eef0d399d0fefe8e7a8130b09700e
NewWorldVulture <49806198+NewWorldVulture@users.noreply.github.com>
2020-01-27 02:59:30 -0800

    Full Rewrite
    Rewrote `Cellular_Automata()` to be a callable class with nice functions and everything. 
    Removed unneeded imports.

--- Cell_Automata.py
+++ Cell_Automata.py
@@ -2,58 +2,96 @@ from PIL import Image
 import PIL
 import itertools
 import random
-import math
 
-size = 500
-rule = 23976
+class CellularAutomata():
+    def __init__(self, rule_number, image_size=500, automata_input_bits=3, number_of_colors=2,
+                    random_first_row=False):
+        # Highest rule number for a set of settings.
+        self.maximum_rule_number = rule_number ** (automata_input_bits ** number_of_colors)
+        self.automata_input_bits = automata_input_bits
+        self.number_of_colors = number_of_colors
+        self.random_first_row = random_first_row
+        # Proportion the image based on number of bits in `input_size`
+        # e.g. for `input_size` 3, make the pic 2:1. For `input_size` 5, it's 4:1
+        self.image_size = ((automata_input_bits//2+1)*image_size+2, image_size+1)
+        self.rule_number = self.convert_rule_to_base(rule_number)
+        self.rule_dictionary = self.create_rule_dictionary()
 
-# please only input odds. Evens don't work how I want them to yet
-input_size = 5
-number_of_colors = 3
+        self.colors = [(0, 0, 0), (255, 255, 255), (255, 0, 0), (255, 255, 0), (255, 0, 255)]
 
-# turns the rules
-size = ((input_size//2+1)*size+2, size+1)
+    # Converts `n` to base `base`, with a maximum value of...
+    # ... `maximum_rule_number = number_of_colors ** (automata_input_bits ** number_of_colors)`
+    # Anything above that just uses unnecessary computer cycles, as they're...
+    # ... equivalent to `rule_number % maximum_rule_number`
+    def convert_rule_to_base(self, rule_number):
+        if rule_number == 0:
+            return ['0'] * (self.number_of_colors**self.automata_input_bits+self.number_of_colors)
+        if rule_number > self.maximum_rule_number:
+            rule_number = rule_number % self.maximum_rule_number
+        nums = []
+        # Generate each digit of the new rule number in base
+        while rule_number:
+            rule_number, remainder = divmod(rule_number, self.number_of_colors)
+            nums.append(str(remainder))
+            # Complicated. Rule numbers in Cellular Automata are defined by
+        new_rule_number = ''.join(nums)[::-1].zfill(self.number_of_colors**self.automata_input_bits+self.number_of_colors)[::-1]
+        return new_rule_number
 
-def convert_to_base(n, base):
-    if n == 0:
-        return '0'
-    nums = []
-    while n:
-        n, r = divmod(n, base)
-        nums.append(str(r))
-    return ''.join(nums)[::-1].zfill(number_of_colors**input_size+number_of_colors)[::-1]
+    def create_rule_dictionary(self):
+        # list of numbers less than `number_of_colors`. Joined as a String. e.g. `012`
+        numbers_in_base = ''.join([str(integer) for integer in range(self.number_of_colors)])
+        # `rule_input_keys` is a list of all n-ary digits that are `input_size` in length in order...
+        # ... like so: `['000', '001', '002' '020', '021', '022' ...]`
+        # We need one key per possible value for each possible input for the CA generation
+        rule_input_keys = list(map(''.join, itertools.product(numbers_in_base, repeat=self.automata_input_bits)))
 
-rule_formatted = list(convert_to_base(rule, number_of_colors))
+        # Maps the keys produced to the digits of `rule_number`
+        return dict(zip(rule_input_keys, self.rule_number))
 
-# Produces all n-ary digits that are `input_size` in length in order...
-# ...into a list like so: `['000', '001', '010', '011', ...]`
-numbers = ''.join([str(x) for x in range(number_of_colors)])
-keys = list(map(''.join, itertools.product(numbers, repeat=input_size)))
+    def decide_value_by_rule(self, row, cell):
+        # Look at the `automata_input_bits` above the cell we want to color and ...
+        # ... decide what the value of the cell should be based on `rule_dictionary`
+        rule_input = ''.join(self.cellular_automata[row-self.automata_input_bits//2]
+                                                   [cell-self.automata_input_bits//2:cell+self.automata_input_bits//2+1])
+        value = self.rule_dictionary[rule_input]
+        return value
 
-# Maps the keys produced to the digits of `rule_binary`
-rule_rules = dict(zip(keys, rule_formatted))
+    def create_automata_array(self):
+        self.ca_width, self.ca_height = self.image_size
+        # First create an array filled with '0's
+        self.cellular_automata = [['0' for _ in range(self.ca_width)] for _ in range(self.ca_height)]
 
-width, height = size
-cells = [['0' for _ in range(width)] for _ in range(height)]
-origin = int(width/2)+1
-cells[0][origin] = '1'
-#cells[0] = [random.choice(["0","1"]) for _ in range(width)]
-colors = [(0, 0, 0), (255, 255, 255), (255, 0, 0), (255, 255, 0), (255, 0, 255)]
+        # If we have a randomly assigned first row, randomly decide values for each cell
+        if self.random_first_row:
+            self.cellular_automata[0] = [random.choice([str(val) for val in range(self.number_of_colors)]) for _ in range(self.ca_width)]
+        else:
+            # Mark the center pixel as '1' (white) if we don't have a randomly assigned first row
+            self.cellular_automata[0][int(self.ca_width//2)+1] = '1'
 
-def decide_rule(row, cell):
-    rule_input = ''.join(cells[row-input_size//2][cell-input_size//2:cell+input_size//2+1])
-    val = rule_rules[rule_input]
-    return val
+        # The fun stuff. For each cell in each row, decide what the value should be
+        for image_row in range(self.ca_height-1):
+            for row_cell in range(self.ca_width - self.automata_input_bits):
+                cell_value = self.decide_value_by_rule(image_row+1, row_cell+self.automata_input_bits//2)
+                self.cellular_automata[image_row+1][row_cell+1] = cell_value
 
-for r in range(height-1):
-    for c in range(width-input_size):
-        cell = decide_rule(r+1, c+input_size//2)
-        cells[r+1][c+1] = cell
 
-im = Image.new("RGB", size, "#000000")
-pixels = im.load()
-for r in range(height-1):
-    for c in range(width-input_size//2-1):
-        pixels[c, r] = colors[int(cells[r+1][c+1])]
-im.save(f"rule{rule}_{input_size}_bit.png")
-im.show()
+    def create_automata_image(self):
+        self.create_automata_array()
+
+        im = Image.new("RGB", self.image_size, "#000000")
+        cellular_automata_image = im.load()
+        for image_row in range(self.ca_height-1):
+            for row_cell in range(self.ca_width-self.automata_input_bits//2-1):
+                cell_value = self.cellular_automata[image_row+1][row_cell+1]
+                color = self.colors[int(cell_value)]
+                cellular_automata_image[row_cell, image_row+1] = color
+        im.save(f"rule{self.rule_number}_{self.automata_input_bits}_bit.png")
+        im.show()
+
+
+def main():
+    new_automata = CellularAutomata(110)
+    new_automata.create_automata_image()
+
+if __name__ == '__main__':
+    main()
